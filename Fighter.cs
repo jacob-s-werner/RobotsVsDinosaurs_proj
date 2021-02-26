b87453282fdd58cd3fc1611577fcbcf1f7bf29fa
Jacob Werner <jacob.scott.werner@gmail.com>
2021-02-26 14:56:25 -0600

    created public int energyRechargeRate in Fighter Class; Created Attack Method, takes in Fighter enemyFighter and deals damage to them if this has an energyLevel greater than or equal to attackEnergyUsage. If the enemyFighter.health reaches zero then it is killed, or else it shows how much enemyFighter.health is left.

--- Fighter.cs
+++ Fighter.cs
@@ -9,12 +9,47 @@ namespace RobotsVsDinosaurs
     public class Fighter
     {
         public string name;
-        public int health, energyLevel, attackEnergyUsage, attackDamageGiven;
+        public int health, energyLevel, attackEnergyUsage, attackDamageGiven, energyRechargeRate;
 
         public Fighter()
         {
             health = 100;
             energyLevel = 100;
         }
-    }
+        public void Attack(Fighter enemyFighter)
+        {
+            if (energyLevel >= attackEnergyUsage)
+            {
+                Console.WriteLine($"{name} attacks for {attackDamageGiven} damage!");
+                enemyFighter.health -= attackDamageGiven;
+                energyLevel -= attackEnergyUsage;
+                if (enemyFighter.health > 0)
+                {
+                    Console.WriteLine($"{enemyFighter.name} now has {enemyFighter.health} health left.");
+                }
+                else
+                {
+                    Console.WriteLine($"{enemyFighter.name} has fallen!\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{name} has low power and is recharging its battery.");
+                energyLevel += energyRechargeRate;
+            }
+        }
+    } 
+      //        ___                         
+      //       |+ +|                 
+      // ______|{=}|______                 
+      //{                 }        
+      // |               |            
+      // |               |          
+      // |               |            
+      // |**|         |**|                               
+      //     |   |   |            
+      //     |   |   |                               
+      //     |   |   |            
+      //    _|   |   |_
+      //  [_____ | _____]
 }
