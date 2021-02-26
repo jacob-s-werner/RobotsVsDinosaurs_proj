6807ab4e340c1f402ae7bfcfbb760cbef1a6b9ed
Jacob Werner <jacob.scott.werner@gmail.com>
2021-02-26 14:21:32 -0600

    Created Fighter Class; created variables for health, name, and energyLevel. Created constructor to set health and energyLevel to 100 at creation.

---
+++ Fighter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsVsDinosaurs
+{
+    class Fighter
+    {
+        public string name;
+        public int health, energyLevel;
+
+        public Fighter()
+        {
+            health = 100;
+            energyLevel = 100;
+        }
+    }
+}
